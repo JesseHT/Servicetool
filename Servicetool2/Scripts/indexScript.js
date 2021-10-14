22e0b0d4ac3ddbaf9621f488a27c14e69f8c68c4
jesse pardoen <pardoenjesse@gmail.com>
2021-10-14 14:35:27 +0200

    sick, klikcount werkt

--- Servicetool2/Scripts/indexScript.js
+++ Servicetool2/Scripts/indexScript.js
@@ -9,7 +9,7 @@ var i = 0;
 
 //document.getElementById("vragen").appendChild(fullLijst);
 
-vraagRef.orderBy("Klikcount", "desc")
+vraagRef.Where("Categorie = Algemeen").orderBy("Klikcount", "desc").limit(2)
     .get()
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
@@ -55,7 +55,7 @@ vraagRef.orderBy("Klikcount", "desc")
             btn.setAttribute("data-bs-target", "#collapse" + i);
             btn.setAttribute("aria-expanded", "true");
             btn.setAttribute("aria-controls", "collapse" + i);
-            btn.innerHTML = Categorie + " | "+ Vraag;
+            btn.innerHTML = Categorie + " | " + Vraag;
 
             h5.appendChild(btn);
             cardHeader.appendChild(h5);
@@ -106,17 +106,17 @@ vraagRef.orderBy("Klikcount", "desc")
             var klikKnop = document.createElement("button");
             klikKnop.innerHTML = "Heeft deze vraag geholpen?";
             klikKnop.className = "btn btn-primary";
-            klikKnop.setAttribute("onclick", "klikCount('"+Categorie + " | " + Vraag + "');")
+            klikKnop.setAttribute("onclick", "klikCount('" + Categorie + " | " + Vraag + "');")
             kliks.append(klikKnop);
             cardbody.append(kliks);
 
             //
             card.appendChild(collapse);
 
-            
+
 
             //lijst in div zetten
-            document.getElementById("accordion").appendChild(card);
+            document.getElementById("algemeenDiv").appendChild(card);
 
             if (document.getElementById("categorieKeuze").innerText == Categorie) {
                 console.log("goed");
@@ -150,94 +150,12 @@ function openVraag(vraagNaam) {
                 window.scroll(rect.width, rect.y);
 
             }, 200)
-            
-            
+
+
         }
-        
+
     }
 
-}
-
-function zoekFunctie() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('zoekInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementsByClassName("accordion-header");
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (l = 0; l < ul.length; l++) {
-        li = document.querySelectorAll('[id^="heading"]');
-
-        var trefwoorden = document.querySelectorAll('[class^="trefwoordID' + li[l].children[0].children[0].innerHTML + '"]');
-
-        for (i = 0; i < trefwoorden.length; i++) {
-            a = trefwoorden[i].innerHTML;
-            console.log(a);
-            txtValue = a;
-            var ul1 = $(ul[l]).parents()[0];
-
-            console.log(txtValue.toUpperCase().indexOf(filter));
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-
-                ul1.style.display = "";
-                break;
-            } else {
-                ul1.style.display = "none";
-            }
-        }
-    }
-}
-
-function sortFunctie() {
-    var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
-    list = document.getElementById("vraagLijst");
-    switching = true;
-    // Set the sorting direction to ascending:
-    dir = "asc";
-    // Make a loop that will continue until no switching has been done:
-    while (switching) {
-        // Start by saying: no switching is done:
-        switching = false;
-        b = list.getElementsByTagName("li");
-        // Loop through all list-items:
-        for (i = 0; i < (b.length - 1); i++) {
-            // Start by saying there should be no switching:
-            shouldSwitch = false;
-            /* Check if the next item should switch place with the current item,
-            based on the sorting direction (asc or desc): */
-            if (dir == "asc") {
-                if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-                    /* If next item is alphabetically lower than current item,
-                    mark as a switch and break the loop: */
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir == "desc") {
-                if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
-                    /* If next item is alphabetically higher than current item,
-                    mark as a switch and break the loop: */
-                    shouldSwitch = true;
-                    break;
-                }
-            }
-        }
-        if (shouldSwitch) {
-            /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
-            b[i].parentNode.insertBefore(b[i + 1], b[i]);
-            switching = true;
-            // Each time a switch is done, increase switchcount by 1:
-            switchcount++;
-        } else {
-            /* If no switching has been done AND the direction is "asc",
-            set the direction to "desc" and run the while loop again. */
-            if (switchcount == 0 && dir == "asc") {
-                dir = "desc";
-                switching = true;
-            }
-        }
-    }
 }
 
 function klikCount(vraagNaam) {
